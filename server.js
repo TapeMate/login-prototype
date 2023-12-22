940c32a54c929d936a7c7ad06bfe1b3846fd8042
TapeMate <admin@tapemate.onmicrosoft.com>
2023-12-22 11:54:39 +0100

    added different login functions.

--- server.js
+++ server.js
@@ -1,26 +1,56 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+
+const initializePassport = require("./passport-config");
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
 // storing user data in local variable instead of database - dev only
 const users = [];
 
 // setup to use ejs syntax
 app.set("view-engine", "ejs");
-
 // defining way to access req variable via data from templates
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // setup rout for rendering
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "Kyle" });
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
 });
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
@@ -41,5 +71,19 @@ app.post("/register", async (req, res) => {
   }
   console.log(users);
 });
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    res.redirect("/");
+  }
+  return next();
+}
 
 app.listen(3000);
