3b054338684068f1cc010e29ed92b3e1a4a4440f
TapeMate <admin@tapemate.onmicrosoft.com>
2023-12-22 09:21:43 +0100

    added name to server. added ejs output on name.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
+// setup to use ejs syntax
+app.set("view-engine", "ejs");
+
 // setup rout for rendering
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { name: "Kyle" });
 });
 
 app.listen(3000);
